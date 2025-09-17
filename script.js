4312b9ddeb35ad3c2e728317336bcddeff29291c
neha chintawar <nehachintawar2002@gmail.com>
2025-09-17 16:11:59 +0530

    random quote generator

---
+++ script.js
@@ -0,0 +1,13 @@
+  const quotes = [
+      { text: "The best way to get started is to quit talking and begin doing.", author: "Walt Disney" },
+      { text: "Don’t let yesterday take up too much of today.", author: "Will Rogers" },
+      { text: "It’s not whether you get knocked down, it’s whether you get up.", author: "Vince Lombardi" },
+      { text: "If you are working on something exciting, it will keep you motivated.", author: "Steve Jobs" },
+      { text: "Success is not in what you have, but who you are.", author: "Bo Bennett" }
+    ];
+
+    function generateQuote() {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      document.getElementById("quote").innerText = quotes[randomIndex].text;
+      document.getElementById("author").innerText = "- " + quotes[randomIndex].author;
+    }
